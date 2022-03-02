c79bbfef7a4c10c9fdf01b28aca3509200082d59
suhita4 <63366417+suhita4@users.noreply.github.com>
2022-03-02 13:22:22 +0530

    Retry

--- api/product.js
+++ api/product.js
@@ -1,4 +1,4 @@
-const express = require("express");
+/*const express = require("express");
 const res = require("express/lib/response");
 const router = express.Router();
 
@@ -6,7 +6,7 @@ const router = express.Router();
  * GET prodyct list.
  * 
  * @return product list | empty
- */
+
 router.get("/", async (req, res) => {
   try {
     res.json({
@@ -19,4 +19,21 @@ router.get("/", async (req, res) => {
   }
 });
 
-module.exports = router;
+module.exports = router; */
+
+const app = require('express')();
+const { v4 } = require('uuid');
+
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+module.exports = app;
